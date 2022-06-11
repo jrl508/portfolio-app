bf3a9a0c3c67ba3648e2c6598bec1e3af0db9aa6
Ricardo Lafontant <Ricky@SaaShas-MacBook-Pro.local>
2022-06-11 10:16:36 -0400

    setup hero banner

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Navbar } from './Navbar'
+import styles from '../styles/Layout.module.css'
 
 
 export const Layout = ({children}) => {
     return (
-        <>
+        <div  className={styles.layout_container}>
         <Navbar /> 
-        <main>
+        <main className={styles.layout_main}>
             {children}
         </main>
-        </>
+        </div>
     )
 };
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Head from 'next/head'
 import React from 'react';
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h3>HOME PAGE</h3>
-    </div>
+    <>
+      <div className={styles.cta_hero}>
+        <div className={styles.cta_hero_text}>
+          <h1>Jean Lafontant</h1>
+          <p>Full Stack Developer</p>
+          <button>Let's Create Something</button>
+        </div>
+    
+      </div>
+    </>
   )
 }
